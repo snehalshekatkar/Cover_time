b0f3862612061b5f5c13c94c71f21df923c2e9f8
Snehal Shekatkar <snehalshekatkar@gmail.com>
2017-09-28 22:34:06 +0530

    Adding a simulation for an extended star

---
+++ simulation_extended_star.py
@@ -0,0 +1,48 @@
+""" 
+This code takes an extended star with arm lengths 3, 2, 2 respectively and spreads a message starting from a central node. Message is passed to a random neighbor with a probability phi.
+
+"""
+
+import graph_tool.all as gt
+import numpy as np
+
+# Construct the extended star with a given arm lengths
+g = gt.Graph(directed = False)
+g.add_edge_list([(0, 1), (1, 2), (2, 3), (0, 4), (4, 5), (0, 6), (6, 7)])
+
+# Create vertex properties to keep track of whether a vertex has a message or not
+state_old = g.new_vertex_property('int')
+state_new = g.new_vertex_property('int')
+
+# Initialize the states: 0 and 1 represent the absense and the presence of the message 
+for v in g.vertices():
+    state_old[v] = 0
+
+# Put a message on a central node
+state_old[0] = 1
+
+# Specify the probability of spread
+phi = 0.3
+
+# Start the spread
+tot_infected_nodes = 1
+t = 0
+
+while tot_infected_nodes < g.num_vertices():
+    # Spread the message to the neighbours
+    for v in g.vertices():
+        if state_old[v] == 1:
+            for nbr in v.out_neighbours():
+                if np.random.random() < phi:
+                    state_new[nbr] = 1
+                    tot_infected_nodes += 1
+
+    # Update the states
+    for v in g.vertices():
+        state_old[v] = state_new[v]
+
+    # Update the time variable
+    t += 1
+
+    
+print("Total time required: ", t)    
